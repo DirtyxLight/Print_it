56e89dd2fb3f27ff706cc9b2b23645e0834b725e
Robin <miguet.robin@gmail.com>
2023-01-25 19:47:53 +0100

    Correction HTML + CSS pour iontégré différent elem + JS en cours (pb utilisation array)

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,4 +1,4 @@
-const slides = [
+/*const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -16,3 +16,27 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+*/
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('const.json');
+const slides = await reponse.json();
+
+
+const bannerImg = document.createElement("img");
+bannerImg.src = slides.image;
+
+const nameLine = document.createElement("p");
+nameLine.innerText = slides.tagLine;
+
+const carousel = document.querySelector('#banner');
+
+banner.appendChild(bannerImg);
+banner.appendChild(nameLine);
+
+
+
+
+const next = document.querySelector('.arrow_right');
+const previous = document.querySelector('.arrow_left');
+
+let index = 0;
