192e062e71f52ceef367a461eceea6bc50048262
Robin <miguet.robin@gmail.com>
2023-01-27 02:45:17 +0100

    Réalisation du carrousel pour images et paragraphe =tout ok + Dot implémenter=faire dot_selected + synchro dot avec carrousel

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,36 +1,73 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "./assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "./assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "./assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "./assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
-const bannerImg = document.createElement("img");
-bannerImg.src = slides.image;
-
-const nameLine = document.createElement("p");
-nameLine.innerText = slides.tagLine;
-
-const carousel = document.querySelector('#banner');
-
-banner.appendChild(bannerImg);
-banner.appendChild(nameLine);
-
-
+];
 
 
 const next = document.querySelector('.arrow_right');
 const previous = document.querySelector('.arrow_left');
-
 let index = 0;
+const dots = document.querySelector('.dots');
+
+for (let i in slides) {
+	const nav = document.createElement("div");
+	dots.appendChild(nav)
+	nav.className = "dot";
+
+	console.log(nav);
+}
+
+const dot = document.getElementsByClassName('dot');
+function color(){
+	if (dot === currentSlide(index)){
+		dot.className = "dot_selected";
+	}
+}
+
+console.log(dot);
+
+
+
+
+
+const bannerImg = document.createElement("img");
+banner.appendChild(bannerImg);
+bannerImg.className = "banner-img";
+const currentSlide = n => {
+	bannerImg.src = slides[n].image;
+}
+currentSlide(index)
+
+const nameLine = document.createElement("p");
+banner.appendChild(nameLine);
+const currentTagLine = n => {
+	nameLine.innerHTML = slides[n].tagLine;
+}
+currentTagLine(index)
+
+
+next.addEventListener('click', (e) => {
+	index = (index + 1) % slides.length
+	currentSlide(index)
+	currentTagLine(index)
+})
+
+previous.addEventListener('click', (e) => {
+	index = (index + slides.length - 1) % slides.length
+	currentSlide(index)
+	currentTagLine(index)
+})
